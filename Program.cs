92047a282f2f82359003958f86e10bfa015eea16
s19981 <s19981@pjwstk.edu.pl>
2020-10-27 13:03:06 +0100

    Pierwszy commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://www.pja.edu.pl");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var html = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+@[a-z.]+");
+
+                MatchCollection matches = regex.Matches(html);
+                foreach(var i in matches)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Koniec!");
+        }
+    }
+}
